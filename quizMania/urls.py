bc33690459a6ca76e216ab1183b5fcd737d7d65f
Kaushik Jadav <kaushikjadav602@gmail.com>
2018-08-01 20:53:53 +0530

    Update urls.py

--- quizMania/urls.py
+++ quizMania/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('login/', views.login),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document.root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
