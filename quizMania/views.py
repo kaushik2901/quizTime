958c3354f5d711005827ce14062d5a111178c5b4
Kaushik Jadav <kaushikjadav602@gmail.com>
2018-08-10 13:02:54 +0530

    Update views.py

--- quizMania/views.py
+++ quizMania/views.py
@@ -192,10 +192,10 @@ def Distance(request):
             
             try:
                 R = 6373.0
-                lat1 = radians(lat1)
-                lon1 = radians(lon1)
-                lat2 = radians(lat2)
-                lon2 = radians(lon2)
+                lat1 = radians(float(lat1))
+                lon1 = radians(float(lon1))
+                lat2 = radians(float(lat2))
+                lon2 = radians(float(lon2))
                 dlon = lon2 - lon1
                 dlat = lat2 - lat1
                 a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
