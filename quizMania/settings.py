d2814b2dadda15d6540012c7fd311936979d42b6
Kaushik <kaushikjadav602@gmail.com>
2018-08-25 11:57:12 +0530

    gdal lib path added

--- quizMania/settings.py
+++ quizMania/settings.py
@@ -145,3 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, './media/')
 MEDIA_URL = '/media/'
+
+
+GDAL_LIBRARY_PATH=/app/.heroku/vendor/lib/libgdal.so
+GEOS_LIBRARY_PATH=/app/.heroku/vendor/lib/libgeos_c.so
