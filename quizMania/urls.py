bd372d1f2392bcc62826680165e829a20226881c
Kaushik Jadav <kaushikjadav602@gmail.com>
2018-09-06 12:48:42 +0530

    Update urls.py

--- quizMania/urls.py
+++ quizMania/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('distance/', views.Distance),
     path('road_api/', views.RoadApi),
     path('tables/', views.RoadTable),
+    path('upload_image/', views.Image_Upload),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
